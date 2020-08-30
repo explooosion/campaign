40e9a15581b33993623d883ee066326704d72598
explooosion <ta7382@gmail.com>
2020-08-30 15:20:58 +0000

    Deploying to gh-pages from master 2a0d53b6b123a003a3ed8569b14eab1a1d8d1330

---
+++ precache-manifest.c172a3fd746d7133305e05fdde8afbaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562cb140cb768e48a8a8d3305e80619b",
+    "url": "/campaign/index.html"
+  },
+  {
+    "revision": "ee1a614c0cc312035c77",
+    "url": "/campaign/static/js/2.cb04ae0a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/campaign/static/js/2.cb04ae0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0710cf9ef317c3e54810",
+    "url": "/campaign/static/js/main.b3a0963f.chunk.js"
+  },
+  {
+    "revision": "d6e34d5c9558a5869b8f",
+    "url": "/campaign/static/js/runtime-main.1f44209e.js"
+  }
+]);
